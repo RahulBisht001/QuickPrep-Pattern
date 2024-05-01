925802c34fccb6212037d6951cb71fa6fdf4a1c3
RahulB <rahulbisht1012@gmail.com>
2024-05-01 11:02:30 +0530

    update

---
+++ Special Question/Hashing/Q1.java
@@ -0,0 +1,36 @@
+/*
+ * 
+ * Given a 0-indexed integer array nums of length n and an
+ * integer k, return the number of pairs (i, j) such that:
+ * 0 <= i < j <= n - 1 and
+ * nums[i] * nums[j] is divisible by k.
+ * 
+ */
+
+import java.util.HashMap;
+
+class Solution {
+    public long countPairs(int[] nums, int k) {
+
+        long cnt = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; ++i) {
+            int a = gcd(nums[i], k);
+
+            for (int key : map.keySet()) {
+                if (((a % k) * (key % k) % k) == 0)
+                    cnt += map.get(key);
+            }
+            map.put(a, map.getOrDefault(a, 0) + 1);
+        }
+
+        return cnt;
+    }
+
+    private int gcd(int a, int b) {
+        if (a < b)
+            return gcd(b, a);
+        return b == 0 ? a : gcd(b, a % b);
+    }
+}
